7126834a1c6a660423716185d45fd3d16f3c5501
greenalex519 <32032234+greenalex519@users.noreply.github.com>
2018-05-06 00:55:59 +1000

    deleted test home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :users
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'items#index'
 end
