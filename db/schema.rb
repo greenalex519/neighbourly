40b9a360cfff65dfad1d1701320f86ef1edd3d81
greenalex519 <32032234+greenalex519@users.noreply.github.com>
2018-05-06 14:58:40 +1000

    added devise

--- db/schema.rb
+++ db/schema.rb
@@ -15,21 +15,5 @@ ActiveRecord::Schema.define(version: 2018_05_04_065637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
