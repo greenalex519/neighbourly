307e84be1fab86af81774743d37cd8a80c02ae52
greenalex519 <32032234+greenalex519@users.noreply.github.com>
2018-05-09 14:04:55 +1000

    lol

--- db/migrate/20180509033304_add_confirmable_to_users.rb
+++ db/migrate/20180509033304_add_confirmable_to_users.rb
@@ -5,7 +5,7 @@ class AddConfirmableToUsers < ActiveRecord::Migration[5.2]
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
     end
+    add_index  :users, :confirmation_token, :unique => true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_05_09_033304) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
