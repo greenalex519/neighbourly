5da3f5495fa822fe9b84737052df698468871261
greenalex519 <32032234+greenalex519@users.noreply.github.com>
2018-05-10 14:55:26 +1000

    added stripe checkout payment functionality

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'mini_magick', '~> 4.3'
 
 gem 'fog-aws'
 
+gem 'stripe'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
 end
